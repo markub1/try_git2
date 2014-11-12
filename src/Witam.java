b2cb51d5e6c55ca8b1e212275474c4303bec2fd2
Arletaxx <arletawasiuk@wp.pl>
2014-11-12 14:34:11 +0100

    Została dodana nie duża klasa"Witam", wyświetlająca na ekranie komunikat "Witam serdecznie na Środowiskach Systemów Informatycznych".

---
+++ src/Witam.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author lab702-03
+ */
+public class Witam {
+    public static void main (String[] args){
+        System.out.println("Witam serdecznie na Środowiskach Systemów Informatycznych");
+    }
+    
+}
