e4a32f932c6984432e2123f96c8710c2e8032947
Stoczi <michalsto@poczta.fm>
2014-11-12 15:55:52 +0100

    Przedstawią się

---
+++ src/Wojtekk.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author LAB702-01
+ */
+public class Wojtekk {
+   public static void main (String[] args){
+    System.out.println("Cześć jestm W");
+}
+}
